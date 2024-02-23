eb9c0bfbe340a7770196edfec9c95a926cbd7669
Nicolas11Garcia <nicolasgarciajimenez12@gmail.com>
2024-02-23 17:06:11 -0300

    up

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+
+    theme: {
+        extend: {
+            colors: {
+                primary: '#37AB46',
+                negro: '#191919',
+                primary_hover: '#0064DA',
+                plomo: '#666666',
+                badget: '#F0F9FF',
+                negro_hover: '#474747',
+            }
+        }
+
+    }
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx,html}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#37AB46',
+                negro: '#191919',
+                primary_hover: '#0064DA',
+                badget: '#F0F9FF',
+                plomo: '#666666',
+                negro_hover: '#2B2B2B',
+            }
+        }
+    },
+    plugins: [],
+}
